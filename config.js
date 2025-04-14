24152b6016c7f50e055b31bdbbf4b900e1f0cd94
berzslv <37150635+berzslv@users.noreply.github.com>
2025-04-14 23:56:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const { PublicKey } = require("@solana/web3.js");
 
 module.exports = {
   STAKING_PROGRAM_ID: new PublicKey("EnGhdovdYhHk4nsHEJr6gmV5cYfrx53ky19RD56eRRGm"),
-  TOKEN_MINT: new PublicKey("12KQqSdN6WEuwo8ah1ykfUPAWME8Sy7XppgfFun4N1D5"),
+  TOKEN_MINT: new PublicKey("59TF7G5NqMdqjHvpsBPojuhvksHiHVUkaNkaiVvozDrk"),
   TOKEN_PROGRAM_ID: new PublicKey("TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"),
   NETWORK: "https://api.devnet.solana.com"
 };
